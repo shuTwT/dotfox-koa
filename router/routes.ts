b40e837d661b650383b6c3b502765f174b0a1933
shuTwT <1324073644@qq.com>
2024-05-17 17:22:57 +0800

    chore: update

--- router/routes.ts
+++ router/routes.ts
@@ -2,7 +2,6 @@ import type Koa from "koa";
 import Router from "koa-router";
 import { str2num, parseQuery } from "./api/utils.js";
 import { apiRouter } from "./api/api.js";
-import { SwaggerRouter } from "koa-swagger-decorator";
 
 const router = new Router<Koa.DefaultState, Koa.Context>();
 
@@ -10,24 +9,10 @@ router.get("/", async (ctx, next) => {
   ctx.body = "项目已成功运行,请通过前端访问"
 });
 
-const swaggerRouter = new SwaggerRouter<Koa.DefaultState, Koa.Context>({
-  spec: {
-    info: {
-      title: "Example API Server",
-      version: "v1.0",
-    },
-  },
-  swaggerHtmlEndpoint: "/api/swagger/index.html",
-  swaggerJsonEndpoint: "/api/swagger/api-docs",
-});
-
-swaggerRouter.swagger();
 
 export const createRoutes = (app: Koa<Koa.DefaultState, Koa.Context>) => {
   app
     .use(router.routes())
-    .use(swaggerRouter.routes())
-    .use(swaggerRouter.allowedMethods())
     .use(apiRouter.routes())
     .use(apiRouter.allowedMethods())
    
