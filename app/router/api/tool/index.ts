8f7cfeab276a9a418fa7a9cbd2bff4c73084111a
shuTwT <1324073644@qq.com>
2024-05-24 15:42:19 +0800

    chore: update

--- app/router/api/tool/index.ts
+++ app/router/api/tool/index.ts
@@ -8,18 +8,18 @@ const router = new Router<DefaultState, Context>({
 
 
 const genTablecontroller = new GenTableController()
-router.get("/", genTablecontroller.list);
-router.get("/db", genTablecontroller.db);
-router.get("/column/:tableId", genTablecontroller.column);
-router.post("/import-table",genTablecontroller.importTable);
-router.post("/create/table",genTablecontroller.createTable);
-router.get("/preview/:tableId", genTablecontroller.previewTable);
-router.get("/download/:tableName", genTablecontroller.downloadTable);
-router.get("/gen-code/:tableName", genTablecontroller.genCode);
-router.get("/sync-db/:tableName", genTablecontroller.syncDb);
-router.get("/batch-gen-code", genTablecontroller.batchGenCode);
-router.get("/:tableId", genTablecontroller.selectTable);
-router.put("/:tableId", genTablecontroller.editTable);
-router.delete("/:tableId", genTablecontroller.deleteTable);
+router.get("/gen", genTablecontroller.list);
+router.get("/gen/db", genTablecontroller.db);
+router.get("/gen/column/:tableId", genTablecontroller.column);
+router.post("/gen/import-table",genTablecontroller.importTable);
+router.post("/gen/create/table",genTablecontroller.createTable);
+router.get("/gen/preview/:tableId", genTablecontroller.previewTable);
+router.get("/gen/download/:tableName", genTablecontroller.downloadTable);
+router.get("/gen/gen-code/:tableName", genTablecontroller.genCode);
+router.get("/gen/sync-db/:tableName", genTablecontroller.syncDb);
+router.get("/gen/batch-gen-code", genTablecontroller.batchGenCode);
+router.get("/gen/:tableId", genTablecontroller.selectTable);
+router.put("/gen/:tableId", genTablecontroller.editTable);
+router.delete("/gen/:tableId", genTablecontroller.deleteTable);
 
 export { router as toolRouter}
